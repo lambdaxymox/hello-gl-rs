735c02e600aa1d10b3d4d6bc4f369a15b0bce6bd
Stallmanifold <stallmanifold@gmail.com>
2018-02-25 01:44:04 +0000

    Define function make_buffers for making opengl data buffers.

--- src/main.rs
+++ src/main.rs
@@ -2,68 +2,24 @@ extern crate glfw;
 extern crate gl;
 
 use glfw::{Action, Context, Key};
-use gl::types::GLfloat;
+use gl::types::{
+    GLsizeiptr, GLenum, GLuint, GLsizei, GLfloat, GLushort
+};
+use std::mem;
 
 
-#[derive(Copy, Clone, Debug)]
-struct Color {
-    r: GLfloat,
-    g: GLfloat,
-    b: GLfloat,
-    a: GLfloat,
-}
 
-impl Color {
-    fn new() -> Color {
-        Color {
-            r: 0.0, g: 0.0, b: 0.0, a: 1.0
-        }
-    }
+/*
+ *  Data used to seed our vertex array and element array buffers.
+ */
+const G_VERTEX_BUFFER_DATA: [GLfloat ; 8] = [
+    -1.0, -1.0,
+     1.0, -1.0,
+    -1.0,  1.0,
+     1.0,  1.0
+];
 
-    fn from_colors(r: GLfloat, g: GLfloat, b: GLfloat, a: GLfloat) -> Color {
-        Color {
-            r: r, g: g, b: b, a: a
-        }
-    }
-}
-
-struct Screen {
-    color: Color,
-}
-
-impl Screen {
-    fn new(color: Color) -> Screen {
-        Screen {
-            color: color,
-        }
-    }
-
-    fn update(&mut self, interval: GLfloat) {
-        self.color.b += interval;
-        if self.color.b > 1.0 {
-            self.color.b = 0.0;
-            self.color.g += interval;
-        }
-        if self.color.g > 1.0 {
-            self.color.g = 0.0;
-            self.color.r += interval;
-        }
-        if self.color.r > 1.0 {
-            self.color.r = 0.0;
-            self.color.g = 0.0;
-            self.color.b = 0.0;
-        }
-    }
-}
-
-#[inline]
-unsafe fn update_screen(screen: &mut Screen) {
-    screen.update(0.1);
-    gl::ClearColor(
-        screen.color.r, screen.color.g, screen.color.b, screen.color.a
-    );
-    gl::Clear(gl::COLOR_BUFFER_BIT);
-}
+const G_ELEMENT_BUFFER_DATA: [GLushort; 4] = [0, 1, 2, 3];
 
 fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
     match event {
@@ -74,12 +30,43 @@ fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
     }
 }
 
+fn render(window: &mut glfw::Window) {
+    unsafe {    
+        gl::ClearColor(0.0, 0.0, 0.0, 0.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+    }
+    // Swap front and back buffers
+    window.swap_buffers();
+}
+
+fn make_buffer(target: GLenum, buffer_data: &mut [GLfloat]) -> GLuint {
+    let mut buffer = 0;
+    let buffer_size = (buffer_data.len() * mem::size_of::<GLfloat>()) as GLsizeiptr;
+
+    unsafe {
+        gl::GenBuffers(1, &mut buffer);
+        gl::BindBuffer(target, buffer);
+        gl::BufferData(
+            target,
+            buffer_size,
+            mem::transmute(&buffer_data[0]),
+            gl::STATIC_DRAW
+        );
+    }
+
+    buffer
+}
+
+fn make_resources() {
+
+}
+
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
     // Create a windowed mode window and its OpenGL context
-    let (mut window, events) = glfw.create_window(640, 480, "Hello, GL!", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window.");
+    let (mut window, events) = glfw.create_window(400, 300, "Hello GL!", glfw::WindowMode::Windowed)
+                                   .expect("Failed to create GLFW window.");
 
     // Make the window's context current
     window.make_current();
@@ -88,17 +75,10 @@ fn main() {
     // Load the OpenGl function pointers.
     gl::load_with(|symbol| { window.get_proc_address(symbol) as *const _ });
 
-    let mut screen = Screen:: new(Color::new());
-
     // Loop until the user closes the window
     while !window.should_close() {
-        // Swap front and back buffers
-        window.swap_buffers();
+        render(&mut window);
 
-        unsafe {
-            update_screen(&mut screen);
-        }
-        
         // Poll for and process events
         glfw.poll_events();
         for (_, event) in glfw::flush_messages(&events) {
